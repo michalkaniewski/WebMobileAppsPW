baec1cedcdf64b965a44aaabe10bb3f09a375770
Michał Kaniewski <m.kaniewski@4semantics.pl>
2020-10-12 09:16:14 +0200

    change

--- app.py
+++ app.py
@@ -5,44 +5,19 @@ supported_languages = ["pl", "cz", "es", "en-gb", "en"]
 
 @app.route('/')
 def index():
-    lang = request.accept_languages.best_match(supported_languages)
-    if lang == "pl":
-        msg = "Witaj"
-    elif lang == "cz":
-        msg = "Ahoj"
-    elif lang == "es":
-        msg = "Hola"
-    elif lang == "en-gb":
-        msg = "Delighted to meet you"
-    else:
-        msg = "Howdy"
-    
-    return f"<h1>{msg}.</h1>"
-
-@app.route('/secret')
-def secret():
-    return "No entry!", 403
-
-@app.route('/brew/<beverage>')
-def brew(beverage):
-    if beverage == "coffee":
-        return '''<pre>
-    I am a teapot
-       _...._
-     .'  _ _ `.
-    | ."` ^ `". _,
-    \_;`"---"`|//
-      |       ;/
-      \_     _/
-        `"""`
-        </pre>''', 418
-    if beverage == "beer":
-        return "Silly you..." 
-    if beverage == "vodka":
-        return "на здоро́вье"
-    if beverage == "tea":
-        return "Which one? Green, red, black..."
-    return "Huh?", 400
+    return """
+    <!doctype html>
+    <head>
+        <link rel="stylesheet" href="/static/main.css"></link>
+    </head>
+    <body>
+        <h3>Witaj</h3>
+        <p>Formularz</p>
+        <form>
+        	<input type="text"></input>
+        	<input type="text"></input>
+        </form>
+    </body>"""
     
 if __name__ == '__main__':
     app.run(threaded=True, port=5000, debug=True)
